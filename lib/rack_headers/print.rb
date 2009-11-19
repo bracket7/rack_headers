01331252a0f0527b2f8fabe6d96246508ed59646
Ryan Daigle <ryan.daigle@gmail.com>
2009-11-19 11:13:16 -0500

    Better log formatting

--- lib/rack_headers/print.rb
+++ lib/rack_headers/print.rb
@@ -14,23 +14,25 @@ module Rack
         # Defaults
         self.request_headers ||= ['If-Modified-Since']
         self.response_headers ||= ['Last-Modified']
-        self.logger ||= Rails.configuration.logger if defined?(Rails)
+        self.logger ||= defined?(RAILS_DEFAULT_LOGGER) ? RAILS_DEFAULT_LOGGER : Logger.new(STDOUT)
       end
 
       def call(env)
-        output(request_headers, env)
+        output(request_headers, env, '==> Request Headers: ')
         status, headers, response = @app.call(env)
-        output(response_headers, headers)
+        output(response_headers, headers, '<== Response Headers: ')
+        logger.info("\n\n")
         [status, headers, response]
       end
 
       private
 
-      def output(keys, headers)
-        logger.info keys.collect do |key|
-          "#{key}: #{headers[key.to_s]}"
-        end.join(', ')
+      def output(keys, headers, prefix = 'Print Headers: ')
+        str = keys.collect do |key|
+          headers.key?(key.to_s) ? "#{key}: \"#{headers[key.to_s]}\"" : nil
+        end.compact.join(', ')
+        logger.info "#{prefix}#{str}"
       end
     end
   end
-end # Warden
+end
